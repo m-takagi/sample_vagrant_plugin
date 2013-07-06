ca204ded5ca613ef0f5ce84318e6ad0df51385d5
TAKAGI Masahiro <matakagi@gmail.com>
2013-07-07 07:14:07 +0900

    Add parsing command-line option

--- lib/sample_vagrant_plugin/command.rb
+++ lib/sample_vagrant_plugin/command.rb
@@ -1,9 +1,15 @@
 module SampleVagrantPlugin
   class Command < Vagrant.plugin("2", "command")
     def execute
+      opts = OptionParser.new do |o|
+        o.banner = "Usage: vagrant free-memory [--help]"
+      end
+
+      argv = parse_options(opts)
+
       command = "grep MemFree /proc/meminfo | awk '{print $2}'"
 
-      with_target_vms(@argv) do |machine|
+      with_target_vms(argv) do |machine|
         if machine.state.id != :running
           @env.ui.info("#{machine.name} is not running.")
           next
