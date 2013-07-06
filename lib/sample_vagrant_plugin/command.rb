73efeeecbac668414e9dfa41d4bfb450b5e779de
TAKAGI Masahiro <matakagi@gmail.com>
2013-07-07 06:50:02 +0900

    Show state of the machine

--- lib/sample_vagrant_plugin/command.rb
+++ lib/sample_vagrant_plugin/command.rb
@@ -1,7 +1,8 @@
 module SampleVagrantPlugin
   class Command < Vagrant.plugin("2", "command")
     def execute
-      puts "Hello!"
+      machine = @env.machine(:default, :virtualbox)
+      puts machine.state.id.to_s
       return 0
     end
   end
