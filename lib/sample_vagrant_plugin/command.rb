cb0c27a46c2857c2554adf640ca116c408b63cab
TAKAGI Masahiro <matakagi@gmail.com>
2013-07-07 07:08:55 +0900

    Support multimachine environments

--- lib/sample_vagrant_plugin/command.rb
+++ lib/sample_vagrant_plugin/command.rb
@@ -1,15 +1,17 @@
 module SampleVagrantPlugin
   class Command < Vagrant.plugin("2", "command")
     def execute
-      machine = @env.machine(:default, :virtualbox)
-      if machine.state.id != :running
-        @env.ui.error("Machine must be running.")
-        return 1
-      end
-
       command = "grep MemFree /proc/meminfo | awk '{print $2}'"
-      machine.communicate.execute(command) do |type, data|
-        @env.ui.info(data)
+
+      with_target_vms(@argv) do |machine|
+        if machine.state.id != :running
+          @env.ui.info("#{machine.name} is not running.")
+          next
+        end
+
+        machine.communicate.execute(command) do |type, data|
+          @env.ui.info(data)
+        end
       end
 
       return 0
