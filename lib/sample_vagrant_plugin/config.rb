a750503dd212a801db77f9037791eefd25426d25
TAKAGI Masahiro <matakagi@gmail.com>
2013-07-09 05:46:53 +0900

    Implement basic configuration merging rules

--- lib/sample_vagrant_plugin/config.rb
+++ lib/sample_vagrant_plugin/config.rb
@@ -1,5 +1,15 @@
 module SampleVagrantPlugin
   class Config < Vagrant.plugin("2", "config")
     attr_accessor :value
+
+    def initialize
+      super
+
+      @value = UNSET_VALUE
+    end
+
+    def finalize!
+      @value = nil if @value == UNSET_VALUE
+    end
   end
 end
